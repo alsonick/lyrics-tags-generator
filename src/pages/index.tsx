171f175062d2426417b456b04625c7b3bbe92aac
Nicholas <contactnicholas81@gmail.com>
2025-04-19 10:08:29 +0100

    improve seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,18 +97,23 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
         <title>{seoTitle} | YouTube</title>
-        <meta name="description" content={seoDescription} />
+        <meta name="description" content={seoDescription} key="desc" />
         <meta property="og:title" content={`${seoTitle} | YouTube`} />
+        <meta name="theme-color" content="#f54bff" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="twitter:title" content={`${seoTitle} | YouTube`} />
         <meta name="twitter:description" content={seoDescription} />
         <meta property="og:image" content="/tags.png" />
         <meta name="twitter:image" content="/tags.png" />
+        <meta property="twitter:site" content="@nick" />
         <meta property="og:url" content="tags.notnick.io" />
+        <meta property="og:site_name" content="tags.notnick.io" />
+        <meta property="twitter:creator" content="@heynickn" />
         <meta property="og:type" content="website" />
         <meta name="twitter:image" content="/tags.png" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:description" content={seoDescription} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="600" />
       </Head>
